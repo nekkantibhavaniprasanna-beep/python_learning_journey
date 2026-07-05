e849463d8a36601ec7be64ab272fca452c1e1817
Bhavani Prasanna Nekkanti <nekkantibhavaniprasanna@gmail.com>
2026-07-05 18:46:14 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,6 @@
+a,b=int(input("Enter two numbers separated by a space: ").split())
+print("The sum of the two numbers is:", a + b)
+print("The difference of the two numbers is:", a - b)
+print("The product of the two numbers is:", a * b)
+if b != 0:
+    print("The quotient of the two numbers is:", a / b)
